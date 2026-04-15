a511a972e935427fd631e9264333a6816bd5bf5f
Ankit-K-Jha <kumarankit7471234@gmail.com>
2026-04-15 10:54:06 +0530

    Update Selectionsort.c

--- sorting/Selectionsort.c
+++ sorting/Selectionsort.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int length,i,j,temp,p;
+    int length, i, j, temp, p;
     printf("Enter the length of the array: ");
-    scanf("%d",&length);
+    scanf("%d", &length);
     int a[length];
     printf("Enter the array entities: \n");
-    for(i=0;i<length;i++)
+    for (i = 0; i < length; i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d", &a[i]);
     }
     printf("\nArray elements are: \n");
-    for(i=0;i<length;i++)
+    for (i = 0; i < length; i++)
     {
-        printf("%d ",a[i]);
+        printf("%d ", a[i]);
     }
     printf("\nSelection sort example: \n");
     // for(i=0;i<length;i++)
@@ -29,22 +29,22 @@ int main()
     //         }
     //     }
     // }
-    for(i=0;i<length-1;i++)
+    for (i = 0; i < length - 1; i++)
     {
-        p=i;
-        for(j=i;j<length;j++)
+        p = i;
+        for (j = i; j < length; j++)
         {
-            if(a[j]<a[p])
+            if (a[j] < a[p])
             {
-                p=j;
+                p = j;
             }
         }
-        temp=a[p];
-        a[p]=a[i];
-        a[i]=temp;
+        temp = a[p];
+        a[p] = a[i];
+        a[i] = temp;
     }
-    for(i=0;i<length;i++)
+    for (i = 0; i < length; i++)
     {
-        printf("%d ",a[i]);
+        printf("%d ", a[i]);
     }
 }

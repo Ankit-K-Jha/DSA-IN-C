05b90798344f0b58ef33b048412fc266a53c9d7a
Ankit-K-Jha <kumarankit7471234@gmail.com>
2026-09-01 22:31:10 +0530

    Added comments in searching techniques and modified some codes

--- Searching/Binary_Search.c
+++ Searching/Binary_Search.c
@@ -1,50 +1,91 @@
-#include<stdio.h>
-void BinarySearch(int  arr[],int length,int key)
+#include <stdio.h>
+
+// Function to perform Binary Search
+void BinarySearch(int arr[], int length, int key)
 {
-    int i,mid,end=length-1,start=0,found=0,pstn;
-    for(i=0;i<length;i++)
+    int i, mid;
+    int end = length - 1;
+    int start = 0;
+    int found = 0;
+    int pstn;
+
+    // Repeat the search until the element is found
+    // or the search range becomes invalid
+    for (i = 0; i < length; i++)
     {
-        mid=((start+end)/2);
-        if(arr[mid]==key)
+        // Find the middle element of the current search range
+        mid = (start + end) / 2;
+
+        // Check if the middle element is the required key
+        if (arr[mid] == key)
         {
             found++;
-            pstn=mid+1;
+
+            // Store position (position starts from 1)
+            pstn = mid + 1;
+
+            // Element found, so stop searching
             break;
         }
-        else if(arr[mid]>key)
+
+        // If middle element is greater than key,
+        // search in the left half
+        else if (arr[mid] > key)
         {
-            end=mid-1;
+            end = mid - 1;
         }
+
+        // If middle element is smaller than key,
+        // search in the right half
         else
         {
-            start=mid+1;
+            start = mid + 1;
         }
     }
-    if(found)
+
+    // Display the result
+    if (found)
     {
-        printf("%d is found at %d position.",key,pstn);
+        printf("%d is found at %d position.", key, pstn);
     }
     else
-    printf("Element is not found!!");
+    {
+        printf("Element is not found!!");
+    }
 }
+
 int main()
 {
-    int i,length,key;
+    int i, length, key;
+
+    // Take the size of the array from the user
     printf("Enter the length of the array: \n");
-    scanf("%d",&length);
+    scanf("%d", &length);
+
+    // Create an array of the given size
     int a[length];
+
+    // Take array elements as input
     printf("Enter the array elements: \n");
-    for(i=0;i<length;i++)
+    for (i = 0; i < length; i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d", &a[i]);
     }
+
+    // Display the array elements
     printf("\nArray elements are: \n");
-    for(i=0;i<length;i++)
+    for (i = 0; i < length; i++)
     {
-        printf("%d ",a[i]);
+        printf("%d ", a[i]);
     }
+
+    // Take the element to be searched
     printf("\nEnter the element to search: \n");
-    scanf("%d",&key);
+    scanf("%d", &key);
+
+    // Perform Binary Search
     printf("Binary Search example: \n");
-    BinarySearch(a,length,key);
+    BinarySearch(a, length, key);
+
+    return 0;
 }
--- Searching/Linear_Search.c
+++ Searching/Linear_Search.c
@@ -1,39 +1,70 @@
-#include<stdio.h>
-void LinearSearch(int arr[],int length,int key)
+#include <stdio.h>
+
+// Function to perform Linear Search
+void LinearSearch(int arr[], int length, int key)
 {
-    int i,found=0,pstn;
-    for(i=0;i<length;i++)
+    int i, found = 0, pstn;
+
+    // Traverse the array from beginning to end
+    for (i = 0; i < length; i++)
     {
-        if(key==arr[i])
+        // Check whether the current element matches the key
+        if (key == arr[i])
         {
-            found++;pstn=i;
+            // Increment found when the element is found
+            found++;
+
+            // Store the index of the found element
+            pstn = i;
+            break;
         }
     }
-    if(found==1)
+
+    // If the element is found exactly once, display its position
+    if (found == 1)
     {
-        printf("The element %d is found at: %d postition.",key, (pstn+1));
+        // Add 1 because array index starts from 0
+        printf("The element %d is found at: %d position.", key, (pstn + 1));
     }
     else
-    printf("Element Not Found!!");
+    {
+        // Element was not found
+        printf("Element Not Found!!");
+    }
 }
+
 int main()
 {
-    int length,key,i;
+    int length, key, i;
+
+    // Take the size of the array from the user
     printf("Enter the size of the array: \n");
-    scanf("%d",&length);
+    scanf("%d", &length);
+
+    // Create an array of the given size
     int a[length];
+
+    // Take array elements as input
     printf("Enter the array elements: \n");
-    for(i=0;i<length;i++)
+    for (i = 0; i < length; i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d", &a[i]);
     }
+
+    // Display the array elements
     printf("\nArray elements are: \n");
-    for(i=0;i<length;i++)
+    for (i = 0; i < length; i++)
     {
-        printf("%d ",a[i]);
+        printf("%d ", a[i]);
     }
+
+    // Take the element to be searched
     printf("\nEnter the element to search: \n");
-    scanf("%d",&key);
+    scanf("%d", &key);
+
+    // Call Linear Search function
     printf("Linear Search : \n");
-    LinearSearch(a,length,key);
+    LinearSearch(a, length, key);
+
+    return 0;
 }
